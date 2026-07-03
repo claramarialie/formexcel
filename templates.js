cbb0bb37685738adc5a68c95a6d2f1fe14ca73f9
Clara Lie <129947255+claramarialie@users.noreply.github.com>
2026-07-03 14:20:21 +0700

    basic

---
+++ templates.js
@@ -0,0 +1,27 @@
+export const templates = {
+
+  opsi1: {
+
+    nama: "Opsi 1",
+
+    aspectRatio: 1,
+
+    maxImages: 4,
+
+    layout: "A"
+
+  },
+
+  opsi2: {
+
+    nama: "Opsi 2",
+
+    aspectRatio: 3 / 4,
+
+    maxImages: 6,
+
+    layout: "B"
+
+  }
+
+};
